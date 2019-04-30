f58491237bba2c398d36d259168866c5dac5f785
Scarlet Villasana <s.claret.v@gmail.com>
2019-04-30 02:51:50 -0400

    avance

---
+++ menu.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg bg-black fixed-top">
+  <div class="container navbar-container">
+    <a class="navbar-brand d-xl-none d-lg-none d-md-block d-sm-block" href="#">
+      <img src="assets/img/logotipo-autofin-blanco.svg" width="150" height="45" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fa fa-bars text-white" aria-hidden="true"></i>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto w-100 nav-justified ">
+        <li class="nav-item">
+          <a class="nav-link af-link text-white" href="#">PORTAL CES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link af-link text-white" href="#">TRINIDAD</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link af-link text-white" href="#">PANAL</a>
+        </li>
+        <li class="nav-item d-none d-lg-block d-xl-block">
+          <a class="navbar-brand" href="#">
+            <img src="assets/img/logotipo-autofin-blanco.svg" width="150" height="45" alt="">
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link af-link text-white" href="#">ARAÑA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link af-link text-white" href="#">MI AUTOFIN</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link af-link text-white" href="#">FUERZA</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
