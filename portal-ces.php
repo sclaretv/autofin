8ab11655d735356ac5d3de030e11948cf949aa88
Scarlet Villasana <s.claret.v@gmail.com>
2019-05-02 03:30:29 -0400

    productos avance

--- portal-ces.php
+++ portal-ces.php
@@ -9,7 +9,7 @@
           <div class="producto_portal_ces div_productos">
 
             <div class="row">
-              <div class="col-lg-1 iconos-productos">
+              <div class="col-lg-1 iconos-productos d-none d-lg-block d-xl-block">
                 <ul class="list-unstyled my-auto">
 				  <li><img src="assets/icons/autofin-icono-web-3.svg"  width="45" height="45" alt=""  aria-hidden=true></li>
 				  <li><img src="assets/icons/autofin-icono-web-4.svg"  width="45" height="45" alt=""  aria-hidden=true></li>
@@ -30,6 +30,17 @@
               	<div class="text-productos">
               		<p>Aplicación móvil donde podrás evaluar en 5 minutos y con solo 4 datos a tu cliente, acelerando el proceso de venta.</p>
               	</div>
+
+              	<div class="d-xl-none d-lg-none d-md-block d-sm-block">
+              		<ul class="list-inline text-center">
+					  <li class="list-inline-item"><img src="assets/icons/autofin-icono-web-3.svg"  width="40" height="40" alt=""  aria-hidden=true></li>
+					  <li class="list-inline-item"><img src="assets/icons/autofin-icono-web-4.svg"  width="40" height="40" alt=""  aria-hidden=true></li>
+					  <li class="list-inline-item"><img src="assets/icons/autofin-icono-web-5.svg"  width="40" height="40" alt=""  aria-hidden=true></li>
+					  <li class="list-inline-item"><img src="assets/icons/autofin-icono-web-6.svg"  width="40" height="40" alt=""  aria-hidden=true></li>
+					  <li class="list-inline-item"><img src="assets/icons/autofin-icono-web-7.svg"  width="40" height="40" alt=""  aria-hidden=true></li>
+					</ul>
+              	</div>
+              	
 
               </div>
             </div>
