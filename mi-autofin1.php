23e54a4ebe9d8a807941979204ab3648f1e0b142
Scarlet Villasana <s.claret.v@gmail.com>
2019-05-04 14:29:50 -0400

    ava

---
+++ mi-autofin1.php
@@ -0,0 +1,100 @@
+    
+    <div class="container productos">
+      <!-- Sección productos: mi autofin -->
+      <div class="row">
+        <div class="col-lg-1">          
+        </div> 
+
+        <div class="col-lg-10">
+          <div class="producto_miautofin div_productos">
+
+            <div class="row">
+              <div class="col-lg-1 iconos-productos d-none d-lg-block d-xl-block">
+                <ul class="list-unstyled my-auto">
+                  <li class="pb-2">
+                    <a href="https://www.youtube.com/watch?v=B1tQeVZVsdU">
+                      <img src="assets/icons/autofin-icono-web-3.svg"  width="45" height="45" alt=""  aria-hidden=true>
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="https://play.google.com/store/apps/details?id=com.mavimovil.autofin" target="_blank">
+                      <img src="assets/icons/autofin-icono-web-4.svg"  width="45" height="45" alt=""  aria-hidden=true>
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="https://itunes.apple.com/us/app/mi-autofin/id1253130290?ls=1&mt=8" target="_blank">
+                      <img src="assets/icons/autofin-icono-web-6.svg"  width="45" height="45" alt=""  aria-hidden=true>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-lg-5 slider-productos">
+                <div class="float-right">
+                  
+                </div>
+                <div id="carousel-miautofin" class="carousel slide carousel_productos" data-ride="carousel">
+                  
+                  <div class="carousel-inner">
+                    <div class="carousel-item item-carousel active">
+                      <img src="assets/img/AUTOFIN-MIAUTOFIN-1.png" class="d-block w-50 mx-auto" alt="">
+                    </div>
+                    <div class="carousel-item item-carousel ">
+                      <img src="assets/img/AUTOFIN-MIAUTOFIN-2.png" class="d-block w-50 mx-auto" alt="">
+                    </div>
+                    <div class="carousel-item item-carousel ">
+                      <img src="assets/img/AUTOFIN-MIAUTOFIN-3.png" class="d-block w-50 mx-auto" alt="">
+                    </div>
+                  </div>
+                </div>
+                <div class="text-center">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carousel-miautofin" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-miautofin" data-slide-to="1"></li>
+                    <li data-target="#carousel-miautofin" data-slide-to="2"></li>
+                  </ol>
+                </div>
+              </div>
+              <div class="col-lg-6 descripcion-productos">
+
+                <div class="logo-productos">
+                  <img src="assets/img/logotipo-miautofin-blanco.svg" width="195" height="80" alt="">
+                </div>
+
+                <div class="text-productos">
+                  <p>Aplicación móvil que entrega un medio de contacto permanente con la cartera de clientes para renovación, nuevas ventas y pagos de cuota instantáneos.</p>
+
+                  <a href="#novedades" class="btn btn-sm btn-autofin text-button text-white">NOVEDADES</a>
+                </div>
+
+                <div class="d-xl-none d-lg-none d-md-block d-sm-block">
+                  <ul class="list-inline text-center">
+                    <li class="list-inline-item">
+                      <a href="https://www.youtube.com/watch?v=B1tQeVZVsdU" >
+                        <img src="assets/icons/autofin-icono-web-3.svg"  width="40" height="40" alt=""  aria-hidden=true>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href="https://play.google.com/store/apps/details?id=com.mavimovil.autofin" target="_blank">
+                        <img src="assets/icons/autofin-icono-web-4.svg"  width="40" height="40" alt=""  aria-hidden=true>
+                      </a>
+                    </li>
+                    <li class="list-inline-item">
+                      <a href="https://itunes.apple.com/us/app/mi-autofin/id1253130290?ls=1&mt=8" target="_blank">
+                        <img src="assets/icons/autofin-icono-web-6.svg"  width="40" height="40" alt=""  aria-hidden=true>
+                      </a>
+                    </li>
+                  </ul>
+                </div>                
+
+              </div>
+            </div>
+          </div>
+        </div> 
+
+        <div class="col-lg-1">
+        </div> 
+
+      </div>
+
+    </div>
