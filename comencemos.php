1b2878c556e8f1803ca5962c9f975a1c1401ba32
Scarlet Villasana <s.claret.v@gmail.com>
2019-05-05 10:08:03 -0400

    fghj

--- comencemos.php
+++ comencemos.php
@@ -1,28 +1,14 @@
     
-    <div class="container comencemos">
-      <!-- Sección comencemos -->
-      <div class="row">
-        <div class="col-lg-2">          
-        </div> 
+  <div class="container comencemos">
+    
+      <div class="div-comencemos" id="video-comencemos">
+      	<div class="embed-responsive embed-responsive-16by9">
+	        <iframe id="video-autofin" class="embed-responsive-item" src="https://www.youtube.com/embed/WaFeIHV4C_U?rel=0" allowfullscreen ></iframe>
+	      </div>
+      </div> 
 
-        <div class="col-lg-8">
-          <div class="div-comencemos" id="video-comencemos">
-          	<div class="embed-responsive embed-responsive-16by9">
-			  <iframe id="video-autofin" class="embed-responsive-item" src="https://www.youtube.com/embed/WaFeIHV4C_U?rel=0" allowfullscreen on-click="fullscreen('video-autofin')"></iframe>
-			</div>
-          </div> 
+      <div class="text-comencemos">
+				<p>CONOCE NUESTRAS SOLUCIONES TECNOLÓGICAS, QUE HACEN QUE NUESTRO SERVICIO SEA DE VANGUARDIA</p>
+			</div> 
 
-          	<div class="text-comencemos">
-    				<p>CONOCE NUESTRAS SOLUCIONES TECNOLÓGICAS, QUE HACEN QUE NUESTRO SERVICIO SEA DE VANGUARDIA</p>
-    			</div> 
- 
-        </div> 
-
-        <div class="col-lg-2">
-        </div> 
-
-      </div>
-
-
-       
-    </div>
+  </div>
